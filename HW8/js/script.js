8c24116749d4385edf90f157f3b64f8e2e8bcd8c
JohnMersereau <71894672+JohnMersereau@users.noreply.github.com>
2020-12-14 14:37:53 -0500

    Added comments
    Added comments

--- HW8/js/script.js
+++ HW8/js/script.js
@@ -1,10 +1,19 @@
-var tiles = [];
+//John Mersereau
+//John_Mersereau@student.uml.edu
+//Student of UMass Lowell in course 91.61 GUI Programming I
+//Created on 12/14/20
+//Sources:
+//Image and array files given in assignment handout.
+
+var tiles = []; //Holds the game's tiles.
 var tiles_index = 0;
 var placed_tiles = 0;
 
 var starting_tile = 0;
 var new_word = true;
 
+
+//Setting board size and bonus squares.
 const tiles_per_row = 15;
 const number_of_rows = 1;
 const double_word = [2, 12];
@@ -17,9 +26,12 @@ var score = 0;
 
 initialize();
 
+//Creates / Sets up a new game.
 function initialize(){
-  var piece_array_index;
+  //Grabbing the game window made in the html file.
   const gameWindow = document.getElementById("scrabbleWindow");
+
+  var piece_array_index;
   var board;
   var hand;
   var controls;
@@ -28,6 +40,8 @@ function initialize(){
   var td;
   var element;
 
+
+  //Reset values.
   tiles_index = 0;
   placed_tiles = 0;
   tiles_in_hand = 0;
@@ -35,6 +49,7 @@ function initialize(){
   new_word = true;
   score = 0;
 
+  //Filling the tiles array with all the tiles, with correct distribution.
   for(var x = 0; x < 26; x++){
     piece_array_index = (x + 10).toString(36).toUpperCase();
     for(y = 0, z = Object.values(ScrabbleTiles[piece_array_index])[1]; y < z; y++){
@@ -42,6 +57,7 @@ function initialize(){
     }
   }
 
+  //Randomize tile order within tiles array.
   for(var x = 0, size = tiles.length, tileOne, tileTwo, temp; x < 1500; x++){
     tileOne = randNum(size - 1);
     tileTwo = randNum(size - 1);
@@ -51,16 +67,19 @@ function initialize(){
     tiles[tileTwo] = temp;
   }
 
+  //Creating the board.
   board = document.createElement('div');
   board.id = "board";
   board.style.backgroundImage = "url('images/Scrabble_Board_OneLine.png')";
 
+  //Fills the board with the appropriate amount of slots.
+  //Gives each slot their ID, the default classes, and drag/drop attributes and functions.
   for(var x = 0; x < number_of_rows; x++){
     for(var y = 0; y < tiles_per_row; y++){
       image = document.createElement('img');
       image.style.opacity = 0;
       image.id = (x * tiles_per_row) + y;
-      image.src = "images/Scrabble_Tiles/Scrabble_Tile_A.jpg"
+      image.src = "images/Scrabble_Tiles/Scrabble_Tile_A.jpg" //placeholder img.
       image.classList.add("tile", "b_none");
       image.setAttribute("ondrop", "drop(event)");
       image.setAttribute("ondragover", "allow_drop(event)");
@@ -71,24 +90,34 @@ function initialize(){
 
   gameWindow.appendChild(board);
 
+  //Giving each bonus square their appropriate bonus class.
+  //b_none (default) = no bonus.
+  //b_dw = double word.
+  //b_dl = double letter.
+
+  //Double Word bonus.
   for(var x = 0, temp; x < double_word.length; x++){
     temp = document.getElementById(double_word[x]);
     temp.classList.remove("b_none");
     temp.classList.add("b_dw");
   }
 
+  //Double Letter bonus.
   for(var x = 0, temp; x < double_letter.length; x++){
     temp = document.getElementById(double_letter[x]);
     temp.classList.remove("b_none");
     temp.classList.add("b_dl");
   }
 
+  //Creating the hand.
   hand = document.createElement('div');
   hand.id = "hand";
   hand.style.backgroundImage = "url('images/scrabble-tile-holder-406774_640.png')"
 
   gameWindow.appendChild(hand);
 
+  //Creating the slots for the hand, and gives each slot a tile from the tiles array.
+  //Sets their ID and drag/drop attributes and functions.
   for(var image, letter; tiles_in_hand < hand_size; tiles_in_hand++){
     letter = tiles[tiles_index++]
     image = document.createElement('img');
@@ -101,11 +130,15 @@ function initialize(){
     hand.appendChild(image);
   }
 
+  //Creating the controls board. which includes buttons to control the game
+  //and their total score.
+
   controls = document.createElement('div');
   controls.id = "controls";
   tr = document.createElement('tr');
   controls.appendChild(tr);
 
+  //Score card
   td = document.createElement('td');
   element = document.createElement('p');
   element.id = "score";
@@ -113,6 +146,7 @@ function initialize(){
   td.appendChild(element);
   tr.appendChild(td);
 
+  //Submit Word button
   td = document.createElement('td');
   element = document.createElement('button');
   element.id = "submit_word";
@@ -121,6 +155,7 @@ function initialize(){
   td.appendChild(element);
   tr.appendChild(td);
 
+  //New Hand button
   td = document.createElement('td');
   element = document.createElement('button');
   element.id = "new_hand";
@@ -129,6 +164,7 @@ function initialize(){
   td.appendChild(element);
   tr.appendChild(td);
 
+  //Restart button.
   td = document.createElement('td');
   element = document.createElement('button');
   element.id = "reset";
@@ -142,13 +178,21 @@ function initialize(){
   return;
 }
 
-
+//Random Number Generator
+//Takes the max number you want it to return.
+//Returns any number between 0 and max.
 function randNum(max){
   return Math.floor(Math.random() * (Math.floor(max) + 1));
 }
 
+//Gives the user a new hand
+//If the user currently has a full hand:
+//Places the tiles in their hand back into array, randomizes the unused tiles
+//and gives them a new full hand.
+//Otherwise: Nothing.
 function new_hand(){
   if(tiles_in_hand == hand_size){
+    //Places tiles in hand back into the tiles array.
     for(var temp, letter; tiles_in_hand > 0;){
       temp = document.getElementById("h" + (tiles_in_hand-- - 1));
       letter = temp.classList.item(1);
@@ -156,6 +200,7 @@ function new_hand(){
       temp.classList.remove(letter);
     }
 
+    //Randomizes the unused portion of the array.
     for(var x = 0, size = tiles.length, tileOne, tileTwo, temp; x < 1500; x++){
       tileOne = randNum(size - 1 - tiles_index) + tiles_index;
       tileTwo = randNum(size - 1 - tiles_index) + tiles_index;
@@ -165,6 +210,7 @@ function new_hand(){
       tiles[tileTwo] = temp;
     }
 
+    //Deals a new hand.
     for(var temp, letter; tiles_in_hand < hand_size;){
       temp = document.getElementById("h" + tiles_in_hand++);
       letter = tiles[tiles_index++];
@@ -175,14 +221,23 @@ function new_hand(){
   return;
 }
 
+//Scores the most recently placed word.
+//Starts at the first tile placed, then counts all the tiles to the left.
+//The counts all tiles to the right.
+//Bonus letter tiles are applied first, then bonus word tiles.
+//Adds the word's score to the total score and updates the score card.
 function submit_word(){
   var word_score = 0;
   var word_multiplier = 1;
   const score_card = document.getElementById("score");
 
+  //If there wasn't a new word placed, do nothing.
+  //Immediately sets new_word to true if it passes the check in order to
+  //help stop people from submitting their word twice.
   if(new_word){ return; }
   new_word = true;
 
+  //Counts tiles to the left and initial tile.
   for(var x = starting_tile, tile, letter, val; x >= 0;){
     tile = document.getElementById(x--);
     letter = tile.classList.item(2);
@@ -202,6 +257,7 @@ function submit_word(){
     }
   }
 
+  //Counts tiles to the right.
   for(var x = starting_tile + 1, tile, letter, val; x < tiles_per_row;){
     tile = document.getElementById(x++);
     letter = tile.classList.item(2);
@@ -221,15 +277,20 @@ function submit_word(){
     }
   }
 
+  //Applies word bonus and updates the score card.
   word_score = word_score * word_multiplier;
   score += word_score;
   score_card.innerHTML = "Score: " + score;
 
+  //Refills hand.
   refill_hand();
 
   return;
 }
 
+//Refills the user's hand.
+//Checks for any empty slots in the hand, and fills them with tiles from the
+//tiles array.
 function refill_hand(){
   for(var x = 0, tile, letter; x < hand_size && tiles_index < tiles.length;){
     tile = document.getElementById("h" + x++);
@@ -246,17 +307,28 @@ function refill_hand(){
   return;
 }
 
+//Disables defaults that can stop my functions from working.
 function allow_drop(event){
   event.preventDefault();
   return;
 }
 
+//Sets the data that is sent with the drag.
+//Sets the data as the ID of the thing being dragged.
 function drag(event){
   event.dataTransfer.setData("text", event.target.id);
+  return;
 }
 
+//Grabs the data set from the drag function.
+//Checks if the slot is open, and the tile being dragged from the hand exists
+//If those are true, sets the board tile to the tile dragged from the hand,
+//and removes the tile from the hand.
+//If this is the first letter being placed on the board, it can be placed anywhere,
+//otherwise the tile must be next to another tile.
 function drop(event){
   event.preventDefault();
+  //Grab the sent data.
   const tile = document.getElementById(event.dataTransfer.getData('text'));
   var letter;
   const slot = event.target;
@@ -265,16 +337,21 @@ function drop(event){
     (!placed_tiles ||
     (parseInt(slot.id) - 1 >= 0 && document.getElementById(parseInt(slot.id) - 1).style.opacity != 0) ||
     (parseInt(slot.id) + 1 <= tiles_per_row && document.getElementById(parseInt(slot.id) + 1).style.opacity != 0))){
+
+      //Removes tile from hand, and grabs that tile's data.
+      //Removes first to help prevent tile duplication.
       tile.style.visibility = "hidden";
       tiles_in_hand--;
       letter = tile.classList.item(1);
       tile.classList.remove(letter);
 
+      //If this is a new word, set the appropriate values.
       if(new_word){
         new_word = false;
         starting_tile = parseInt(slot.id);
       }
 
+      //Set the board slot to the tile dragged from hand.
       slot.classList.add(letter);
       slot.src = "images/Scrabble_Tiles/Scrabble_Tile_" + letter + ".jpg";
       slot.style.opacity = 1;
@@ -283,9 +360,13 @@ function drop(event){
   return;
 }
 
+//Reset function
+//Deletes everything within the game, then calls the initialize functions
+//to create a new game.
 function reset(){
   while(scrabbleWindow.firstChild){
     scrabbleWindow.removeChild(scrabbleWindow.lastChild);
   }
   initialize();
+  return;
 }
